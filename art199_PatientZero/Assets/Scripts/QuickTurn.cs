ca23c0e5b2cf07ec6f8e0a1f137e1035115675cf
Kevin Huang <huangk11@uci.edu>
2020-02-10 13:55:10 -0800

    Basic quick turn functionality has been implemented.
    If you check the [VRTK_SDKManager] in the Hierarchy, you will see a new script attached to the child object [VRTK_SDKSetups]. The script is called QuickTurn. It works by manipulating the Transform.rotation of the object. There is a serialized field that allows you to adjust how many degrees the quick turn will turn you. Look at the code for more information.
    
    Problems:
    Since the code uses the horizontal axis to "smoothly" turn the player, we turn right now while rotating. We need to figure out some axis that is not being used by any of the motion controls to get this to work.
    
    We also might need to fine tune the degree amount of the quick turn.

---
+++ art199_PatientZero/Assets/Scripts/QuickTurn.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Written by Kevin Huang
+//Last edited: Feburary 10th, 2020
+
+//Since we are developing a virtual reality game, we are using...
+//  the VRTK framework to assist in development. We need to import their
+//  code in order to use it in our project.
+using VRTK;         
+
+/*
+ * This script is meant to help the player quickly turn their camera
+ * For example, if they hit the turn button, they turn about 45 degrees.
+ * If they hold it, they turn as long as they are holding the button.
+ * 
+ * This prevents them from getting motion-sick
+ */
+
+public class QuickTurn : MonoBehaviour
+{
+    //Theroretically, we should be able to turn the player's view by simply
+    //  rotating the transform of the VR Camera component
+
+    //public Transform cameraTransform = VRTK_DeviceFinder.HeadsetCamera();
+
+
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private int quickTurnAmount = 45;
+
+    // Update is called once per frame
+    void Update()
+    {
+        processKeyboardInput();
+    }
+
+    void processKeyboardInput()
+    {
+        //To process input, we will initially test using a keyboard
+        //Eventually though, we want to use the headset's controls...
+
+        if (Input.GetKeyDown("q"))
+        {
+            turnSetAmount(quickTurnAmount);
+        }
+
+        else if (Input.GetKeyDown("e"))
+        {
+            turnSetAmount(quickTurnAmount);
+        }
+
+        else if (Input.GetKeyDown("r"))
+        {
+            turnSetAmount(180);
+        }
+        
+      
+       //we need like a new axis exclusively for moving the player
+       //because this conflicts with the existing arrow keys...
+        player.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        
+        
+    }
+
+    //makes the player rotate x degrees using player.Rotate
+    void turnSetAmount(int degree)
+    {
+        player.Rotate(0, degree, 0);
+    }
+
+    void buttonHeldTurn()
+    {
+        
+    }
+}
