8bff72af1ed5813c689ea0ee501d1b1fae55957e
Kevin Huang <huangk11@uci.edu>
2020-03-06 18:26:55 -0800

    Inspectable and InspectableDNA scripts
    Inspectable allows the player to be able to manipulate their object by rotating in the certain planes of motion as well as scaling them.
    
    InspectableDNA is a derived class that allows the player to manipulate the object in the same manner as if the object were to have the Insepectable script but with the added functionality of being able to combine or separate strands. The strands will ideally start off as separate.

---
+++ art199_PatientZero/Assets/Scripts/Inspectable.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// the Inspectable script basically makes it so you are able to enlarge and shrink objects
+// you will also be able to rotate them left, right, up, and down
+
+// in order to use this script, you can just attach to it any game object...
+// for additional features, it is recommended that you make a new class that inherits from this class
+
+public class Inspectable : MonoBehaviour
+{
+    // these store the default values so that the reset function can use them
+    protected Quaternion originalQuaternion;
+    protected Vector3 originalScale;
+    protected Vector3 originalPosition;
+
+    // the enums facilitate readability in the code below
+    public enum Direction
+    {
+        left,
+        right,
+        up,
+        down,
+        shrink,
+        enlarge
+    }
+
+    // we initialize the default values so the reset function will be able to reset to them
+    public void Start()
+    {
+        originalQuaternion = gameObject.transform.localRotation;
+        originalScale = gameObject.transform.localScale;
+        originalPosition = gameObject.transform.position;
+
+        
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        // this is primarily how the code should be used
+        // feel free to experiment with bindings
+
+        // we will have to change this because the movements will
+        // primarily be activated by an in-game button
+        // the in-game button would probably work best if its held...
+        if (Input.GetKey("q"))
+        {
+            Rotate(Direction.left);
+        }
+
+        if (Input.GetKey("e"))
+        {
+            Rotate(Direction.right);
+        }
+
+        if (Input.GetKey("r"))
+        {
+            ResetObject();
+        }
+
+        
+    }
+
+    // this resets the object's transform to the default values
+    public void ResetObject()
+    {
+        gameObject.transform.localRotation = originalQuaternion;
+        gameObject.transform.localScale = originalScale;
+        gameObject.transform.position = originalPosition;
+    }
+
+    // this either enlarges or shrinks the object
+    public void Scale(Direction direction)
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        Vector3 newScale;
+        switch (direction)
+        {
+            case Direction.enlarge:
+                newScale = new Vector3(currentScale.x + 0.1f, currentScale.y + 0.1f, currentScale.z + 0.1f);
+                gameObject.transform.localScale = newScale;
+                break;
+            case Direction.shrink:
+                newScale = new Vector3(currentScale.x - 0.1f, currentScale.y - 0.1f, currentScale.z - 0.1f);
+                gameObject.transform.localScale = newScale;
+                break;
+        }
+
+    }
+
+    // this allows us to rotate the object
+    public void Rotate(Direction direction)
+    {
+        Quaternion currentRotation = gameObject.transform.localRotation;
+        Quaternion newRotation;
+
+        switch (direction)
+        { 
+            case Direction.left:
+                newRotation = new Quaternion(currentRotation.x - 0.1f, currentRotation.y, currentRotation.z, currentRotation.w);
+                gameObject.transform.localRotation = newRotation;
+                break;
+            case Direction.right:
+                newRotation = new Quaternion(currentRotation.x + 0.1f, currentRotation.y , currentRotation.z, currentRotation.w);
+                gameObject.transform.localRotation = newRotation;
+                break;
+            case Direction.up:
+                newRotation = new Quaternion(currentRotation.x, currentRotation.y + 0.1f, currentRotation.z, currentRotation.w);
+                gameObject.transform.localRotation = newRotation;
+                break;
+            case Direction.down:
+                newRotation = new Quaternion(currentRotation.x, currentRotation.y - 0.1f, currentRotation.z, currentRotation.w);
+                gameObject.transform.localRotation = newRotation;
+                break;
+        }
+    }
+
+    
+    
+}
---
+++ art199_PatientZero/Assets/Scripts/InspectableDNA.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this is a derived class of the Inspectable class
+// the reason that you can't simply use the Inspectable class on the DNA is
+// because this derived class allows the player to "combine" or "separate"
+// the strands...
+
+// in order to use this class...
+// put it on an DNA GameObject
+// there should be two child objects: Left Strand and Right Strand
+// Move them apart from one another...
+// They should be at the same level 
+// Center the parent game object on these two...
+
+public class InspectableDNA : Inspectable
+{
+    private GameObject leftStrand;
+    private GameObject rightStrand;
+
+    // we save the original positions so the strands can be moved back there later
+    private Vector3 leftStrandOriginalPosition;
+    private Vector3 rightStrandOriginalPosition;
+
+    // Start is called before the first frame update
+    private new void Start()
+    {
+        base.Start();
+
+        leftStrand = gameObject.transform.Find("Left Strand").gameObject;
+        rightStrand = gameObject.transform.Find("Right Strand").gameObject;
+
+        leftStrandOriginalPosition = leftStrand.transform.position;
+        rightStrandOriginalPosition = rightStrand.transform.position;
+    }
+
+    // Update is called once per frame
+    private new void Update()
+    {
+        base.Update();
+
+        // the intended way of using the class should be something similar to this
+        // code down here.
+        if (Input.GetKey("a"))
+        {
+            separateDNA();
+        }
+
+        if (Input.GetKey("d"))
+        {
+            combineDNA();
+        }
+    }
+
+    // this moves the DNA strands toward the "center" of their parent object
+    public void combineDNA()
+    { 
+        if (leftStrand && rightStrand)
+        {
+            Vector3 newLeftStrandPos = new Vector3(originalPosition.x + (leftStrand.transform.localScale.x / 2), originalPosition.y , originalPosition.z);
+            Vector3 newRightStrandPos = new Vector3(originalPosition.x - (rightStrand.transform.localScale.x / 2), originalPosition.y, originalPosition.z);
+
+            Vector3 leftStrandNewPosition = Vector3.MoveTowards(leftStrand.transform.position, newLeftStrandPos, 0.01f);
+            Vector3 rightStrandNewPosition = Vector3.MoveTowards(rightStrand.transform.position, newRightStrandPos, 0.01f);
+
+            leftStrand.transform.position = leftStrandNewPosition;
+           rightStrand.transform.position = rightStrandNewPosition;
+        }
+    }
+
+    // this moves the DNA strands back toward their "original" position
+    public void separateDNA()
+    {
+        if (leftStrand && rightStrand)
+        {
+            Vector3 newLeftStrandPos = new Vector3(leftStrandOriginalPosition.x, leftStrandOriginalPosition.y, leftStrandOriginalPosition.z);
+            Vector3 newRightStrandPos = new Vector3(rightStrandOriginalPosition.x, rightStrandOriginalPosition.y, rightStrandOriginalPosition.z);
+
+            Vector3 leftStrandNewPosition = Vector3.MoveTowards(leftStrand.transform.position, newLeftStrandPos, 0.01f);
+            Vector3 rightStrandNewPosition = Vector3.MoveTowards(rightStrand.transform.position, newRightStrandPos, 0.01f);
+
+            leftStrand.transform.position = leftStrandNewPosition;
+            rightStrand.transform.position = rightStrandNewPosition;
+        }
+    }
+
+
+}
