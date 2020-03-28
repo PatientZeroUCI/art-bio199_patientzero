e79a346ed3b6a7d5ea27fae8725daba20ff270f8
brendanmiller10 <bjmille2@uci.edu>
2020-03-27 20:02:12 -0700

    Added Art Assets to Scene
    Added new table and computer models, as well as add the holographic brain to the temp display.

---
+++ art199_PatientZero/Assets/hologram_shift.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables.PhysicsBased;
+
+public class hologram_shift : MonoBehaviour
+{
+   // public VRTK.VRTK_InteractableObject sliderLocation = VRTK_PhysicsSlider.GetControlInteractableObject();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
