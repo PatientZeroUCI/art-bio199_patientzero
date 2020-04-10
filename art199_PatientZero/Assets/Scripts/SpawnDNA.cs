5e82f22993bc72a6ba5bbbe058f1a88ae317a736
Kevin Huang <huangk11@uci.edu>
2020-04-10 00:38:47 -0700

    Implemented the prototype of the DNA puzzle.
    Problems with the current implementation...
    
    The DNA prefabs I created have rotations and positions that might cause problems down the line. I don't have any idea how to reset the transform of a GameObject.
    
    We might have to switch out the text mesh since we are working in VRTK. There's probably something better.
    
    I used a mouse click to cycle through the bases of the DNA.
    
    We might need to add some sort of scene transition after we're done solving the DNA.
    
    I don't know how this scales with bigger DNA.

---
+++ art199_PatientZero/Assets/Scripts/SpawnDNA.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDNA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject leftSegmentPrefab;
+
+    [SerializeField]
+    private GameObject rightSegmentPrefab;
+
+    [SerializeField]
+    private float offsetScalar;
+
+    public bool isRNA = false;
+
+    private bool puzzleComplete = false;
+
+    // store the state of each segment in an array when spawned...
+    GameObject puzzleSegment;
+    GameObject playerSegment;
+
+    TextMesh[] puzzleStrands;
+    TextMesh[] playerStrands;
+
+    void Start()
+    {
+        SpawnDNASegments();
+        RandomizeStrands();
+    }
+
+    private void Update()
+    {
+        puzzleSolved();
+    }
+
+    void puzzleSolved()
+    {
+
+        for (int i = 0; i < puzzleStrands.Length; ++i)
+        {
+            if (puzzleStrands[i].text != OppositeBase(playerStrands[i].text))
+            {
+                return;
+            }
+        }
+
+        if (puzzleComplete == false)
+        {
+            SnapTogether();
+            puzzleComplete = true;
+        }
+    }
+
+
+
+    string OppositeBase(string strandBase)
+    {
+        if (strandBase == "A")
+        {
+            if (isRNA)
+            {
+                return "U";
+            }
+
+            return "T";
+        }
+
+        else if (strandBase == "U")
+        {
+            return "A";
+        }
+
+        else if (strandBase == "T")
+        {
+            return "A";
+        }
+
+        else if (strandBase == "C")
+        {
+            return "G";
+        }
+
+        else if (strandBase == "G")
+        {
+            return "C";
+        }
+
+        return "ERROR";
+    }
+
+    void SnapTogether()
+    {
+        playerSegment.transform.Translate(offsetScalar * Vector3.left, Space.World);
+    }
+
+
+    void SpawnDNASegments()
+    {
+        // Spawns the segment that you have to solve against...
+        puzzleSegment = Instantiate(leftSegmentPrefab, gameObject.transform);
+        puzzleStrands = puzzleSegment.GetComponentsInChildren<TextMesh>();
+        
+        // Spawns the segment that the player has to modify in order to match it... 
+        playerSegment = Instantiate(rightSegmentPrefab, gameObject.transform);
+        playerSegment.transform.Translate(offsetScalar * Vector3.right, Space.World);
+      
+        playerStrands = playerSegment.GetComponentsInChildren<TextMesh>();
+    }
+
+    void RandomizeStrands()
+    {
+        RandomizeBases(puzzleStrands, isRNA);
+
+        // Makes sure the player segment won't end up being the same as the puzzle strand
+        do
+        {
+            RandomizeBases(playerStrands, isRNA);
+        }
+        while (puzzleEqualsPlayer());
+    }
+
+    bool puzzleEqualsPlayer()
+    {
+        for (int i = 0; i < puzzleStrands.Length; ++i)
+        {
+            if (puzzleStrands[i] != playerStrands[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    
+    void RandomizeBases(TextMesh[] segmentStrands, bool isRNA)
+    {
+        for (int i = 0; i < segmentStrands.Length; ++i)
+        {
+            int baseCode = Random.Range(0, 4);
+            segmentStrands[i].text = SelectBase(baseCode, isRNA);
+        }
+    }
+
+    string SelectBase(int baseNumber, bool isRNA)
+    {
+        switch (baseNumber)
+        {
+            case 0:
+                return "A";
+
+            case 1:
+                if (isRNA)
+                {
+                    return "U";
+                }
+
+                return "T";
+
+            case 2:
+                return "C";
+
+            case 3:
+                return "G";
+
+            default:
+                return "ERROR";
+        }
+    }
+}
