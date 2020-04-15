62a5c0962052f55127459da16f206b066373bd01
rayfalke <ronnie_school@yahoo.com>
2020-04-14 17:44:43 -0700

    Worked on spawning menu from interactUse

---
+++ art199_PatientZero/Assets/Scripts/MainScreenController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreenController : MonoBehaviour
+{
+
+    public GameObject nextMenu;
+    public Transform spawnLoc;
+    GameObject spawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void spawnNextMenu()
+    {
+        Debug.Log("pass");
+        spawned = Instantiate(nextMenu, spawnLoc.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+}
