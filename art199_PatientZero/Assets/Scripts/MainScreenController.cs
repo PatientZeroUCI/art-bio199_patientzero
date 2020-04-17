c09747a6a2537d2a39c068d720493e035acba32d
rayfalke <ronnie_school@yahoo.com>
2020-04-17 01:37:33 -0700

    Main Menu Updates
    Figured out/implemented very rough menu utilizing InteractableObject_UnityEvents that allows specific scripts and functions to run when using a specific object

--- art199_PatientZero/Assets/Scripts/MainScreenController.cs
+++ art199_PatientZero/Assets/Scripts/MainScreenController.cs
@@ -1,23 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainScreenController : MonoBehaviour
 {
 
+    public GameObject controller;
     public GameObject nextMenu;
     public Transform spawnLoc;
     GameObject spawned;
+    //private VRTK_InteractUse use;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //controller = GameObject.FindGameObjectWithTag("controllerright");
+        //use = controller.GetComponent<VRTK_InteractUse>();
     }
 
-    public void spawnNextMenu()
+    //public void spawnNextMenu()
+    //{
+    //    Debug.Log("pass");
+    //    spawned = Instantiate(nextMenu, spawnLoc.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    //}
+
+    public void showNextMenu()
     {
-        Debug.Log("pass");
-        spawned = Instantiate(nextMenu, spawnLoc.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+        //if (use.GetUsingObject().name == "NextMenuBtn")
+        //{
+            if (nextMenu.activeSelf)
+                nextMenu.SetActive(false);
+            else
+                nextMenu.SetActive(true);
+        //}
+
+    }
+
+    public void beginGame() {
+        //if (use.GetUsingObject().name == "playbutton")
+        //{
+            SceneManager.LoadScene("Brendan's Scene");
+        //}
+        
     }
 }
