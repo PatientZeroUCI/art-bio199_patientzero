f853e6bd191bff9558de4f54f5abfdc39aeda85e
rayfalke <ronnie_school@yahoo.com>
2020-05-02 18:56:51 -0700

    Starting development of the tool station

---
+++ art199_PatientZero/Assets/Scripts/ToolCenter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolCenter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "Tool"){
+            Destroy(collision.gameObject);
+        }
+    }
+}
