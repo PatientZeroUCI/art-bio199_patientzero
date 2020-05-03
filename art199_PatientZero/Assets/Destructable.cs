3e9d7d6c70981e86fffaf562105b331e71d7743a
brendanmiller10 <bjmille2@uci.edu>
2020-05-02 17:44:59 -0700

    Glass Shatter
    Got the shatter to occur with UnityEvents, currently attached to the ungrab function.

---
+++ art199_PatientZero/Assets/Destructable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+
+    public GameObject destroyedVersion;
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    public void Shatter()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
