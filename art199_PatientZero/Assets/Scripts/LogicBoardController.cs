2d037f90b415a15c8961170198b6706a6760ed60
rayfalke <ronnie_school@yahoo.com>
2020-05-21 17:07:12 -0700

    LineRenderer works with only one snapzone

--- art199_PatientZero/Assets/Scripts/LogicBoardController.cs
+++ art199_PatientZero/Assets/Scripts/LogicBoardController.cs
@@ -10,6 +10,11 @@ using VRTK;
  *      Preferably find some way to include this in one place and use it in snapzone script, instead of having a separate copy of the policy list in every gameobject
  *      Wait now testing with Brendan's scene, I don't think I even needed this to get snapping to work
  * Cannot snap multiple things to the same area
+ * 
+ * 
+ * Now have multiple snap zones, which add to dictionary
+ * 
+ * Items around fall off when center removed
  */
 
 
@@ -20,62 +25,51 @@ public class LogicBoardController : MonoBehaviour
 
     Dictionary<GameObject, List<GameObject>> objects = new Dictionary<GameObject, List<GameObject>>();
     VRTK_SnapDropZone snapScript;
-    public GameObject surf;
+
+    // These will need to be changed to have multiple snap zones
     GameObject obj;
+    GameObject line;
+
+
+    public GameObject LinePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         VRTK_SnapDropZone snapScript = this.gameObject.GetComponent<VRTK_SnapDropZone>();
-        //make sure the object has the VRTK script attached... 
 
-        /*if (GetComponent<VRTK_SnapDropZone>() == null)
-
-        {
-
-            Debug.LogError("LogicBoardController is required to be attached to an Object that has the VRTK_SnapDropZone script attached to it");
-
-            return;
-
-        }*/
-
-        //GetComponent<VRTK_SnapDropZone>().ObjectSnappedToDropZone += new SnapDropZoneEventHandler(ObjectSnappedToDropZone);
     }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
 
-    //}
 
     public void SnappedToCenter(object o, SnapDropZoneEventArgs e)
     {
         obj = e.snappedObject;
         List<GameObject> newList = new List<GameObject>();
         objects.Add(e.snappedObject, newList);
-        
-        //Destroy(surf);
-        //List<GameObject> objs = snapScript.GetHoveringObjects();
-        //Debug.Log(objs[0].name);
-        //GameObject ob = snapScript.GetCurrentSnappedObject();
 
-
-        //Debug.Log(o.name);
-        //GameObject ob = GameObject.Find(obj.name);
-
-        //objects.Add(ob, newList);
     }
 
     public void RemovedFromCenter(object o, SnapDropZoneEventArgs e)
     {
         objects.Remove(e.snappedObject);
+        if (line != null)
+        {
+            Destroy(line);
+        }
     }
 
     public void AddToArea(object o, SnapDropZoneEventArgs e)
     {
-        //check if something is in the center snap first
         if (objects.ContainsKey(obj)){
             objects[obj].Add(e.snappedObject);
+            
+            line = Instantiate(LinePrefab, obj.transform.position, Quaternion.identity);
+            LineRenderer lr = line.GetComponent<LineRenderer>();
+            Vector3[] points = { obj.transform.position, e.snappedObject.transform.position };
+            lr.SetPositions(points);
+            Vector3 v = new Vector3(0f, 0f, 0.15f);
+            line.transform.position = v;
             printdictionary();
         }
 
@@ -86,23 +80,13 @@ public class LogicBoardController : MonoBehaviour
         if (objects.ContainsKey(obj)){
             objects[obj].Remove(e.snappedObject);
             printdictionary();
+            Destroy(line);
         }
 
     }
 
     private void printdictionary()
     {
-        /*
-        string vals = "";
-        objs = snapScript.GetHoveringObjects();
-        foreach (GameObject ob in objs){
-            vals += ob.name + " ";
-        }
-        Debug.Log(obj.name);
-        Debug.Log(vals);
-        */
-
-        
         string vals = "";
         foreach (GameObject ob in objects[obj])
         {
