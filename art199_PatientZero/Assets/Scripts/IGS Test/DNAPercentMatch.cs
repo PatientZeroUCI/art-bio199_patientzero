454a21d64060790d6a91855b9e74683dbc738da9
Conan Truong <truong.conan@gmail.com>
2020-06-03 17:00:56 -0700

    Added DNA ring matching

---
+++ art199_PatientZero/Assets/Scripts/IGS Test/DNAPercentMatch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNAPercentMatch : MonoBehaviour
+{
+    [SerializeField] int matchPercent = 99;
+
+    private TextMesh text;
+
+    private Collider otherRing;
+
+    private void Awake() {
+        text = GetComponentInChildren<TextMesh>();
+        text.color = Color.clear;
+    }
+
+    private void Update() {
+        if (otherRing != null && Quaternion.Angle(otherRing.transform.rotation, transform.rotation) < 20) {
+            text.text = $"{matchPercent}%";
+            text.color = Color.white;
+        } else {
+            text.color = Color.clear;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "DNA Ring") {
+            otherRing = other;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other == otherRing) {
+            otherRing = null;
+        }
+    }
+}
