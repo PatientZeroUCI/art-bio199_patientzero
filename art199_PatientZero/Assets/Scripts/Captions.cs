b2b174064baa7e49846f47bed9bfb4008d2a4cdb
Conan Truong <truong.conan@gmail.com>
2020-07-14 16:42:45 -0700

    Added captions script

---
+++ art199_PatientZero/Assets/Scripts/Captions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Captions : MonoBehaviour {
+    private AudioSource audioSource;
+
+    public TextMeshProUGUI textObject;
+    public TextAsset file;
+
+    private List<(float time, string caption)> captions = new List<(float, string)>();
+    private int currentIndex = 0;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        foreach (string line in file.text.Split('\n')) {
+            if (line != "") {
+                string[] split = line.Split(';');
+                captions.Add((float.Parse(split[0]), split[1]));
+            }
+        }
+    }
+
+    private void Update() {
+        if (audioSource.isPlaying) {
+            if (currentIndex < captions.Count && captions[currentIndex].time < audioSource.time) {
+                textObject.text = captions[currentIndex++].caption;
+            }
+        } else {
+            textObject.text = "";
+        }
+    }
+}
---
+++ art199_PatientZero/Assets/Scripts/StartDemoScript.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+using VRTK.UnityEventHelper;
+
+public class StartDemoScript : MonoBehaviour {
+    private VRTK_InteractableObject_UnityEvents events;
+
+    private void Awake() {
+        events = GetComponent<VRTK_InteractableObject_UnityEvents>();
+        events.OnGrab.AddListener(Grab);
+    }
+
+    private void Grab(object obj, InteractableObjectEventArgs args) {
+        //Destroy(gameObject);
+        SceneManager.LoadScene("Wrap Up");
+    }
+}
