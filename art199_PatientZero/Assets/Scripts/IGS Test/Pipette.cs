373a6f03ccef0912084e6de3a065af988380a48a
brendanmiller10 <bjmille2@uci.edu>
2020-08-22 12:39:51 -0700

    Fixed Floating Pipette
    The issue of the mounted button is no more, swapped the pipette to activate when the player uses their controller instead. This stops the weird interaction we were seeing previously. Also added in hidden Qtip from the model updates.

--- art199_PatientZero/Assets/Scripts/IGS Test/Pipette.cs
+++ art199_PatientZero/Assets/Scripts/IGS Test/Pipette.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Pipette : MonoBehaviour {
     public GameObject contents = null;
+    public VRTK.VRTK_InteractUse controllerUse;
+    public VRTK.VRTK_InteractableObject pipette;
 
     // Spawns copies of the stored object
+    
+    void Update()
+    {
+        if (pipette.IsGrabbed() && controllerUse.IsUseButtonPressed())
+        {
+            DropLiquid();
+        }
+    }
+
     public void DropLiquid() {
         if (contents != null) {
             GameObject liquid = Instantiate(contents);
