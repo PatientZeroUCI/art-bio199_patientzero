561e6ce44727dfb74f0744a2b0f6a849f888cadc
brendanmiller10 <bjmille2@uci.edu>
2020-08-24 14:23:41 -0700

    Rewritten Microscope
    Microscope now has script that chooses what to display based on the tag associated with the input.

---
+++ art199_PatientZero/Assets/MicroscopeDisplay.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MicroscopeDisplay : MonoBehaviour
+{
+    public VRTK.VRTK_SnapDropZone microscope_zone;
+    public GameObject bacteria;
+    public GameObject gram_result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject snapped_object = microscope_zone.GetCurrentSnappedObject();
+        if ( snapped_object != null)
+        {
+            if(snapped_object.tag == "DNA")
+            {
+                bacteria.SetActive(true);
+            }
+            else if(snapped_object.tag == "GRAM")
+            {
+                gram_result.SetActive(true);
+            }
+        }
+        else
+        {
+            if (bacteria.activeSelf)
+            {
+                bacteria.SetActive(false);
+            }
+            else if (gram_result.activeSelf)
+            {
+                gram_result.SetActive(false);
+            }
+        }
+    }
+
+}
