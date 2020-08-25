0eab72f1ef6a55d1ee861a9e105798d74086f553
rgiron1 <36999776+rgiron1@users.noreply.github.com>
2020-08-25 16:00:15 -0700

    Pipet dropping sound

--- art199_PatientZero/Assets/Scripts/IGS Test/Pipette.cs
+++ art199_PatientZero/Assets/Scripts/IGS Test/Pipette.cs
@@ -2,26 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pipette : MonoBehaviour {
+public class Pipette : MonoBehaviour
+{
     public GameObject contents = null;
+    public AudioSource squirting;
     public VRTK.VRTK_InteractUse controllerUse;
     public VRTK.VRTK_InteractableObject pipette;
 
     // Spawns copies of the stored object
-    
+
     void Update()
     {
         if (pipette.IsGrabbed() && controllerUse.IsUseButtonPressed())
         {
             DropLiquid();
         }
+        else
+            squirting.Stop();
     }
 
-    public void DropLiquid() {
-        if (contents != null) {
+    public void DropLiquid()
+    {
+        if (contents != null)
+        {
             GameObject liquid = Instantiate(contents);
             liquid.transform.position = transform.position;
             liquid.SetActive(true);
+            squirting.Play();
         }
     }
 }
