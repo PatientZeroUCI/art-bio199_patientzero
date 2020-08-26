638f710d46ba7c576df15a26bed64fbbdfdb6bf9
brendanmiller10 <bjmille2@uci.edu>
2020-08-26 11:31:02 -0700

    DNA and IGS Results
    Integrated updated DNA sequences, and attached the final one to the completion of the DNA Matching puzzle. DNA Sequence is spawned and can be brought to the existing DNA for testing. Gram Stain result for the IGS also added in for the microscope.

---
+++ art199_PatientZero/Assets/DNAResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNAResult : MonoBehaviour
+{
+    public GameObject test_result;
+    public SpawnDNA puzzle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puzzle.puzzleSolved())
+        {
+            test_result.SetActive(true);
+        }
+    }
+}
