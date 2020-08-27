9cc8e90060cb9846679cae1b60eeeedfc6d60813
Conan Truong <truong.conan@gmail.com>
2020-08-26 19:32:28 -0700

    Squirt bottle now squirts instead of pours

--- art199_PatientZero/Assets/Scripts/IGS Test/SquirtBottle.cs
+++ art199_PatientZero/Assets/Scripts/IGS Test/SquirtBottle.cs
@@ -1,29 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class SquirtBottle : MonoBehaviour
 {
     [SerializeField] GameObject contents = null;
     public AudioSource squirtSound;
 
+    public VRTK_InteractableObject bottle;
+    public VRTK_InteractUse controllerUse;
+
+    void Start() {
+        controllerUse = GameObject.Find("RightHand/Right").GetComponent<VRTK_InteractUse>();
+    }
+
+    float squirting = 0;
+
     private void Update()
     {
-        if (contents != null && Vector3.Angle(transform.rotation * Vector3.forward, Vector3.down) < 30)
+        if (bottle.IsGrabbed() && controllerUse.IsUseButtonPressed()) {
+            if (squirting <= 0) {
+                squirting = 1;
+                squirtSound.Play();
+            }
+        } else if (squirting > 0.2f) {
+            squirting = 0.2f;
+            squirtSound.Stop();
+        }
+
+        if (squirting > 0.2f)
         {
             GameObject liquid = Instantiate(contents);
             liquid.transform.position = transform.position;
             liquid.SetActive(true);
-            if (squirtSound.isPlaying)
-            {
-                //squirtSound.PlayScheduled(.3f);
-            }
-            else
-                squirtSound.Play();
+            //if (squirtSound.isPlaying)
+            //{
+            //    //squirtSound.PlayScheduled(.3f);
+            //}
+            //else
+            //    squirtSound.Play();
         }
         else
         {
-            squirtSound.Stop();
+            //squirtSound.Stop();
         }
+
+        squirting -= Time.deltaTime;
     }
 }
