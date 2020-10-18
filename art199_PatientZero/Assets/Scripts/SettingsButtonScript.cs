11f3a59fa5a4f19b4ad14dc033a48c6f7b384d2a
taroken6 <audiemendaros@yahoo.com>
2020-10-18 13:59:08 -0700

    Settings Button w/ Volume slider

---
+++ art199_PatientZero/Assets/Scripts/SettingsButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsButtonScript : MonoBehaviour
+{
+    public GameObject settingsMenu;
+    private Vector3 initialMenuPos;
+
+    private void Start() {
+        initialMenuPos = settingsMenu.transform.position;
+        settingsMenu.transform.position += new Vector3(0, -10, 0);
+    }
+
+    public void displaySettings() {
+        // Temporary workaround. VRTK's Artificial Slider seems to reinstantiate itself for each time it is reactivated
+        if(settingsMenu.transform.position == initialMenuPos) {
+            settingsMenu.transform.position += new Vector3(0, -10, 0);
+        } else {
+            settingsMenu.transform.position = initialMenuPos;
+        }
+    }
+}
