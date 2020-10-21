ee3f534de3a194bfe9e41808e92921b611d1d787
Ronnie Lucero <ronnie_school@yahoo.com>
2020-10-21 01:45:37 -0700

    Working on the Position Resetter
    More complicated than I thought.

---
+++ art199_PatientZero/Assets/Scripts/PositionResetter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PositionResetter : MonoBehaviour
+{
+    public List<GameObject> objects;
+    public List<Transform> spawns;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        UnityEngine.Debug.Log("Entered");
+        if (collision.gameObject.tag == "Tool" || collision.gameObject.tag == "PetriDish")
+        {
+            collision.gameObject.transform.position = spawns[objects.IndexOf(collision.gameObject)].transform.position;
+        }
+
+    }
+}
