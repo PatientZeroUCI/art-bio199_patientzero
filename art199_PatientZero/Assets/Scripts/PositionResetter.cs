228851e13d7909b4180d7f6f92e152eb6beef9ed
Ronnie Lucero <ronnie_school@yahoo.com>
2020-10-30 18:09:41 -0700

    Tried to add rotation to position resetter, not working

--- art199_PatientZero/Assets/Scripts/PositionResetter.cs
+++ art199_PatientZero/Assets/Scripts/PositionResetter.cs
@@ -9,6 +9,7 @@ public class PositionResetter : MonoBehaviour
 {
     public static List<GameObject> objects = new List<GameObject>();
     public static List<Vector3> vSpawns = new List<Vector3>();
+    public static List<Vector3> rotations = new List<Vector3>();
 
 
     // Start is called before the first frame update
@@ -16,7 +17,8 @@ public class PositionResetter : MonoBehaviour
     {
         foreach (GameObject obj in objects)
         {
-            vSpawns.Add(obj.transform.position);
+            vSpawns.Add(new Vector3(obj.transform.position.x, obj.transform.position.y + 0.1f, obj.transform.position.z));
+            rotations.Add(new Vector3(obj.transform.rotation.eulerAngles.x, obj.transform.rotation.eulerAngles.y, obj.transform.rotation.eulerAngles.z));
         }
     }
 
@@ -30,8 +32,13 @@ public class PositionResetter : MonoBehaviour
     {
         if (collision.gameObject.tag == "Tool" || collision.gameObject.tag == "PetriDish")
         {
-            //collision.gameObject.transform.position = spawns[objects.IndexOf(collision.gameObject)].position;
-            collision.gameObject.transform.position = vSpawns[objects.IndexOf(collision.gameObject)];
+            int index = objects.IndexOf(collision.gameObject);
+
+            collision.gameObject.transform.position = vSpawns[index];
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            collision.gameObject.transform.eulerAngles = rotations[index];
+            
+
         }
 
     }
@@ -39,12 +46,16 @@ public class PositionResetter : MonoBehaviour
     public void setSpawn(GameObject obj)
     {
         objects.Add(obj);
-        vSpawns.Add(obj.transform.position);
+        vSpawns.Add(new Vector3(obj.transform.position.x, obj.transform.position.y + 0.15f, obj.transform.position.z));
+        rotations.Add(new Vector3(obj.transform.rotation.eulerAngles.x, obj.transform.rotation.eulerAngles.y, obj.transform.rotation.eulerAngles.z));
     }
 
     public void removeSpawn(GameObject obj)
     {
+        int index = objects.IndexOf(obj);
         objects.Remove(obj);
-        vSpawns.Remove(obj.transform.position);
+        
+        vSpawns.RemoveAt(index);
+        rotations.RemoveAt(index);
     }
 }
