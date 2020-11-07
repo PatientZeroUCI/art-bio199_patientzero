6eedb1b8eab6c6337612b534171801c04b71b684
jigglequack <45055772+jigglequack@users.noreply.github.com>
2020-11-07 06:19:03 -0800

    added swapping the holograms nose for virus
    returns a virus on the cotton swab and also plays a sneezing sound. I also went back to fix other prefabs

---
+++ art199_PatientZero/Assets/Scripts/Vibrio Isolation/NoseVirusSample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoseVirusSample : MonoBehaviour
+{
+	//public GameObject nose;
+
+	//public var Sneezing : AudioSource;
+
+	void OnCollisionEnter(Collision collider)
+	{
+	    if(collider.gameObject.tag == "tool")
+	    {
+	     	Debug.Log(collider.gameObject.name);
+	        //add the code you want to execute on collision
+	        //to access the Ball gameObject use : col.gameObject
+	    }
+	 }
+
+}
