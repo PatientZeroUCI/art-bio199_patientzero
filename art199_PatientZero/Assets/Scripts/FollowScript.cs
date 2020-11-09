d6aa4a14779cc55e1a1676260b909ad4a62959dc
brendanmiller10 <bjmille2@uci.edu>
2020-11-08 20:18:11 -0800

    Observatory Hologram Projection
    Projects interactable holograms around the player after waiting for any sounds/dialogue that is added at a later point.

--- art199_PatientZero/Assets/Scripts/FollowScript.cs
+++ art199_PatientZero/Assets/Scripts/FollowScript.cs
@@ -9,6 +9,7 @@ public class FollowScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //transform.rotation = Quaternion.Euler(0f, 180f, 0f);
         //position_offset = transform.position - target_object.transform.position;
     }
 
@@ -17,5 +18,7 @@ public class FollowScript : MonoBehaviour
     {
         transform.position = target_object.transform.position;
         transform.rotation = target_object.transform.rotation;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x, 180f + target_object.transform.rotation.y, transform.rotation.z);
     }
 }
---
+++ art199_PatientZero/Assets/Scripts/ObservatoryDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ObservatoryDisplay : MonoBehaviour
+{
+
+    public GameObject observatory_hologram;
+
+    public void WaitDisplay()
+    {
+        StartCoroutine(SleepDisplay());
+    }
+
+    IEnumerator SleepDisplay()
+    {
+        //Anything that needs to be "loaded in" can be placed after the wait. 
+        yield return new WaitForSeconds(3.0f);
+        observatory_hologram.SetActive(true);
+    }
+}
