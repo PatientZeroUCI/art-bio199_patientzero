6e231257c1001578d437eca3bcf775d1e60c429d
Ryan <artlessavian@gmail.com>
2020-11-08 19:18:36 -0800

    Teleporting to and from an observatory

---
+++ art199_PatientZero/Assets/TeleportForwarder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportForwarder : MonoBehaviour
+{
+    public GameObject targetPosition;
+
+    public void DoTeleport()
+    {
+        Transform sdkTransform = VRTK.VRTK_SDKManager.GetLoadedSDKSetup().transform;
+
+        //Debug.Log(targetPosition.transform.position + (this.transform.position - sdkTransform.position));
+
+        //GetComponent<VRTK.VRTK_HeightAdjustTeleport>().Teleport(
+        //    //targetPosition.transform,
+        //    sdkTransform,
+        //    targetPosition.transform.position
+        //);
+
+        GetComponent<VRTK.VRTK_BasicTeleport>().Teleport(
+            sdkTransform,
+            targetPosition.transform.position
+        );
+
+        // i'm just going to set the y coord manually.
+        // yes this is bad
+        sdkTransform.position -= sdkTransform.position.y * Vector3.up; // add new y component.
+        sdkTransform.position += targetPosition.transform.position.y * Vector3.up; // add new y component.
+        Debug.Log(targetPosition.transform.position.y);
+    }
+}
