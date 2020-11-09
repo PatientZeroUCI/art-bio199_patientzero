b69c826afef065aefc51a9b7bb40de9bf40b7754
Victoria W <56736494+Tori-W@users.noreply.github.com>
2020-11-08 20:53:23 -0800

    Added over the shoulder object (for real I think)
    - Added overShoulderSnapDropZone and overShoulderObject to Wrap Up scene
    - Player should be able to reach over their shoulder and grip once (not hold) to grab object
    - Object cannot be placed anywhere and must be returned by pressing grip over the shoulder again
    - Needs to be tested in VR!!!

---
+++ art199_PatientZero/Assets/Scripts/overShoulderSnapZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using VRTK;
+
+public class overShoulderSnapZone : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject snappedObject;
+    public VRTK_SnapDropZone snapZoneObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        snapZoneObject.ForceSnap(snappedObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = VRTK_DeviceFinder.HeadsetTransform().position + offset;
+        if (snapZoneObject.GetCurrentSnappedObject() == snappedObject)
+        {
+            snappedObject.transform.position = transform.position;
+        }
+    }
+}
