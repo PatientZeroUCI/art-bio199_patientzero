b106220e478a0186935dfe3c480588dd15792e3e
taroken6 <audiemendaros@yahoo.com>
2020-11-14 16:48:15 -0800

    Button axis bug fix

--- art199_PatientZero/Assets/VRTK/Examples/ExampleResources/SceneResources/[005 - Interactions] InteractableObjects/Scripts/ControllableReactor.cs
+++ art199_PatientZero/Assets/VRTK/Examples/ExampleResources/SceneResources/[005 - Interactions] InteractableObjects/Scripts/ControllableReactor.cs
@@ -10,9 +10,12 @@
         public Text displayText;
         public string outputOnMax = "Maximum Reached";
         public string outputOnMin = "Minimum Reached";
+        private Vector3 restingPos;
+        private Vector3 maxLimitPos;
 
         protected virtual void OnEnable()
         {
+            restingPos = transform.position;
             controllable = (controllable == null ? GetComponent<VRTK_BaseControllable>() : controllable);
             controllable.ValueChanged += ValueChanged;
             controllable.MaxLimitReached += MaxLimitReached;
@@ -21,6 +24,16 @@
 
         protected virtual void ValueChanged(object sender, ControllableEventArgs e)
         {
+            // Prevent button from going beyond its 'natural' boundaries
+            if(restingPos != null) {
+                if (controllable.AtMinLimit()) {
+                    transform.position = restingPos;
+                }
+                if (controllable.AtMaxLimit()) {
+                    transform.position = maxLimitPos;
+                }
+            }
+
             if (displayText != null)
             {
                 displayText.text = e.value.ToString("F1");
@@ -29,6 +42,10 @@
 
         protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
         {
+            // Get max limit here since idk where else to get it.
+            if(maxLimitPos == new Vector3(0, 0, 0)) {
+                maxLimitPos = transform.position;
+            }
             if (outputOnMax != "")
             {
                 Debug.Log(outputOnMax);
