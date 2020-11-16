1f9ead8c2bb3abfef910e664c8a96e3510ea22a4
jigglequack <45055772+jigglequack@users.noreply.github.com>
2020-11-15 20:09:28 -0800

    Makeshift Egg in Flashlight in Kati Scene

---
+++ art199_PatientZero/Assets/Scripts/Egg Minigame/Egg.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour
+{
+	//code for when the egg enters the flashlight light and changes meshes
+	public Mesh inFlashlight;
+	public Mesh outFlashlight;
+
+    void Start()
+    {
+    	// getting the original mesh of the egg so that when the egg exits the light it reverts 
+    	// back to normal
+        outFlashlight = this.gameObject.GetComponent<MeshFilter>().mesh;
+    }
+
+	void OnCollisionEnter (Collision collision) {
+		if(collision.gameObject.tag == "Flashlight"){
+			this.gameObject.GetComponent<MeshFilter>().mesh = inFlashlight;
+		}
+	}
+
+	void OnCollisionExit (Collision collision){
+		this.gameObject.GetComponent<MeshFilter>().mesh = outFlashlight;
+	}
+}
