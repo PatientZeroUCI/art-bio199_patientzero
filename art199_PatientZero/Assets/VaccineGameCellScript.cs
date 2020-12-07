f0ac3adc0f9c2d0ab1cb918ca46b670e90fa87a9
taroken6 <audiemendaros@yahoo.com>
2020-12-06 16:12:13 -0800

    Vaccine Game 'Good/Bad Shots' script added to Audie's scene

---
+++ art199_PatientZero/Assets/VaccineGameCellScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaccineGameCellScript : MonoBehaviour
+{
+    public bool isHealthyCell;
+    public GameObject bullet;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == bullet.name + "(Clone)") // This works but I feel like there's a 'safer' way to do this.
+        {
+            if (isHealthyCell)
+            {
+                Debug.Log("BAD SHOT: Shot healthy cell!");
+            } 
+            else
+            {
+                Debug.Log("GOOD SHOT: Shot infected cell!");
+            }
+        }
+    }
+}
