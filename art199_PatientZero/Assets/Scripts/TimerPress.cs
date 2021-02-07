52b48cf7fe2e61cea5b378f2ae324f347a119326
Ronnie Lucero <ronnie_school@yahoo.com>
2021-02-06 19:43:29 -0800

    Implemented simple TimePress.cs script for buttons
    Check my scene for a working prototype

---
+++ art199_PatientZero/Assets/Scripts/TimerPress.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerPress : MonoBehaviour
+{
+    public bool activated = false;
+    public float timeToHit = 3.0f;
+    public float offset = 0.5f;
+    float time = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated == true)
+        {
+            time += Time.deltaTime;
+        }
+    }
+
+    public void activateTimer()
+    {
+        activated = true;
+        time = 0f;
+    }
+
+    public void hit()
+    {
+        if (time > timeToHit-offset && time < timeToHit + offset)
+        {
+            UnityEngine.Debug.Log("Pass!");
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Fail!");
+        }
+        activated = false;
+    }
+
+}
