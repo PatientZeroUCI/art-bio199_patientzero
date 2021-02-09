9d695648de6d4a23c1dd33ea1acd1ecfa24ebc9c
Pixsea <jiin@uci.edu>
2021-02-09 06:45:06 -0800

    Added ToolsNoCollide Script and Layer
    Script to be applied to objects with VRTK_InteractableObject Components, can make it so other objects with this script don't collide when left idle or grabbed

---
+++ art199_PatientZero/Assets/Scripts/ToolsNoCollide.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ToolsNoCollide : MonoBehaviour
+{
+    [SerializeField]
+    private bool collideWhileNotGrabbed = false;  // Should a tool be able to collide with other tools when not grabbed
+    [SerializeField]
+    private bool collideWhileGrabbed = false;  // Should a tool be able to interact with other tools when grabbed, should be true for tools like the qtip or egg
+    [SerializeField]
+    private VRTK_InteractableObject obj;  // the Object's VRTK_InteractableObject script
+
+    // Uses layer 9, "ToolsNoCollide", which doesn't collide with other objects witht the tag
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!collideWhileNotGrabbed)
+        {
+            gameObject.layer = 8;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj.IsGrabbed())
+        {
+            if (!collideWhileGrabbed)
+            {
+                gameObject.layer = 8;
+            }
+            else
+            {
+                gameObject.layer = 0;
+            }
+        }
+
+        else
+        {
+            if (!collideWhileNotGrabbed)
+            {
+                gameObject.layer = 8;
+            }
+            else
+            {
+                gameObject.layer = 0;
+            }
+        }
+    }
+}
