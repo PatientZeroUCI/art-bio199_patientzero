1878a61857678b1fdb5f51b36e4e0feac23636e5
Pixsea <jiin@uci.edu>
2021-02-10 18:04:18 -0800

    Made Egg poke minigame detect center
    Syringe must now hit the center of the egg after entering from a certain point, instead of just entering from a certain spot

--- art199_PatientZero/Assets/Scripts/EggSyringe.cs
+++ art199_PatientZero/Assets/Scripts/EggSyringe.cs
@@ -7,7 +7,9 @@ public class EggSyringe : MonoBehaviour
     [SerializeField]
     private GameObject Egg;  // The egg to poke
     [SerializeField]
-    private GameObject PokeZone;  // Where the syringe has to poke
+    private GameObject PokeZone;  // Where the syringe has to enter the egg
+    [SerializeField]
+    private GameObject EggCenter;  // Where the syringe has to poke
 
     private bool goodPoke;  // Whether or not the player has inserted the needle into the PokeZone first
     private bool badPoke;  //Whether or not the player has inserted the needle into the rest of the Egg first
@@ -44,19 +46,25 @@ public class EggSyringe : MonoBehaviour
         // When the needle enters the PokeZone and hasn't entered the rest of the Egg first
         if ((obj.gameObject == PokeZone) && !badPoke)
         {
-            Debug.Log("Good");
+            //Debug.Log("Good");
             goodPoke = true;
-
-            // Call anything you need when a succesful poke is made
         }
 
         // When the needle enters the Egg and hasn't entered the PokeZone first
-        if ((obj.gameObject == Egg) && !goodPoke)
+        else if ((obj.gameObject == Egg) && !goodPoke)
         {
             Debug.Log("Bad");
             badPoke = true;
 
             // Call anything you need when a bad poke is made
+        }
+
+        // When needle enter the PokeZone first then hits the center
+        else if((obj.gameObject == EggCenter) && goodPoke)
+        {
+            Debug.Log("Good");
+
+            // Call anything you need when a succesful poke is made
         }
     }
 
