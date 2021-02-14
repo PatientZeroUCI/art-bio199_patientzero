3b2efa455f5b73090529b1681aae20c7c16c95bf
Ronnie Lucero <ronnie_school@yahoo.com>
2021-02-14 02:35:26 -0800

    Additions to TimerPress.cs and visualizer for time needed to trigger

--- art199_PatientZero/Assets/Scripts/TimerPress.cs
+++ art199_PatientZero/Assets/Scripts/TimerPress.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class TimerPress : MonoBehaviour
 {
     public bool activated = false;
+    public float cap = 0.3f;
     public float timeToHit = 3.0f;
     public float offset = 0.5f;
     float time = 0f;
+    float timePassed;
 
     // Update is called once per frame
     void Update()
     {
         if (activated == true)
         {
-            time += Time.deltaTime;
+            timePassed = Time.deltaTime;
+            time += timePassed;
+            if (time < timeToHit)
+            {
+                this.gameObject.transform.Translate(Vector3.left * (timePassed / (10f * (timeToHit / 3f))));
+            }
+            else
+            {
+                this.gameObject.transform.Translate(Vector3.right * (timePassed / (10f * (timeToHit / 3f))));
+            }
+            
         }
     }
 
@@ -26,6 +38,7 @@ public class TimerPress : MonoBehaviour
 
     public void hit()
     {
+        UnityEngine.Debug.Log(time);
         if (time > timeToHit-offset && time < timeToHit + offset)
         {
             UnityEngine.Debug.Log("Pass!");
