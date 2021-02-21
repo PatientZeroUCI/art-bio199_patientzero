92fa040df80fa9050b441e2fabd1443b1faea3d2
Ronnie Lucero <ronnie_school@yahoo.com>
2021-02-20 18:56:38 -0800

    2D Canvas UI Progress Bar for TimerPress.cs
    May have issues with using on Oculus headsets as I believe Canvases do not work with VR cameras?

--- art199_PatientZero/Assets/Scripts/TimerPress.cs
+++ art199_PatientZero/Assets/Scripts/TimerPress.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerPress : MonoBehaviour
 {
+
+    private Image progressBar;
+    public GameObject canvas;
     public bool activated = false;
     public float cap = 0.3f;
     public float timeToHit = 3.0f;
-    public float offset = 0.5f;
+    public float offsetPercent = 10f;
     float time = 0f;
     float timePassed;
+
 
     // Update is called once per frame
     void Update()
@@ -18,13 +23,16 @@ public class TimerPress : MonoBehaviour
         {
             timePassed = Time.deltaTime;
             time += timePassed;
-            if (time < timeToHit)
+            if (Mathf.Floor((time/timeToHit) % 2) == 0)
             {
-                this.gameObject.transform.Translate(Vector3.left * (timePassed / (10f * (timeToHit / 3f))));
+                progressBar.fillAmount = (time % timeToHit) / timeToHit;
+
+                //this.gameObject.transform.Translate(Vector3.left * (timePassed / (10f * (timeToHit / 3f))));
             }
             else
             {
-                this.gameObject.transform.Translate(Vector3.right * (timePassed / (10f * (timeToHit / 3f))));
+                progressBar.fillAmount = (timeToHit - (time % timeToHit)) / timeToHit;
+                //this.gameObject.transform.Translate(Vector3.right * (timePassed / (10f * (timeToHit / 3f))));
             }
             
         }
@@ -32,6 +40,8 @@ public class TimerPress : MonoBehaviour
 
     public void activateTimer()
     {
+        canvas.SetActive(true);
+        progressBar = this.transform.Find("Progress Bar").gameObject.GetComponent<Image>();
         activated = true;
         time = 0f;
     }
@@ -39,7 +49,13 @@ public class TimerPress : MonoBehaviour
     public void hit()
     {
         UnityEngine.Debug.Log(time);
-        if (time > timeToHit-offset && time < timeToHit + offset)
+        float iteration = Mathf.Floor((time / timeToHit) % 2);
+
+
+
+        float alteredTimeToHit = timeToHit * iteration + 1;
+
+        if (progressBar.fillAmount >= (1 - (offsetPercent/100f)))
         {
             UnityEngine.Debug.Log("Pass!");
         }
@@ -48,6 +64,7 @@ public class TimerPress : MonoBehaviour
             UnityEngine.Debug.Log("Fail!");
         }
         activated = false;
+        canvas.SetActive(false);
     }
 
 }
