5d2a3a4e8920844e26cab86caa6d22cb58283d15
brendanmiller10 <bjmille2@uci.edu>
2021-02-23 16:25:20 -0800

    Snap Rotation & Testing Room Scale
    Set up a rotatable hex tile using a placeholder made in Blender. Rotation on the object used is very important, needs to be zeroed out and initially in the correct rotation to rotate along the proper axis. Scaling scene created and adjusted according to Game Design doc on unity scale.

---
+++ art199_PatientZero/Assets/Scripts/ControllerRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ControllerRotation : MonoBehaviour
+{
+
+    public VRTK_ControllerEvents right_hand;
+    public GameObject rotatableObject;
+
+    private VRTK_InteractableObject grab_hand;
+    private void Start()
+    {
+        grab_hand = this.gameObject.GetComponent<VRTK_InteractableObject>();
+    }
+
+    void Update()
+    {
+        if (grab_hand.IsGrabbed() && grab_hand.GetGrabbingObject() == rotatableObject)
+        {
+            //if(right_hand.)
+        }
+    }
+}
