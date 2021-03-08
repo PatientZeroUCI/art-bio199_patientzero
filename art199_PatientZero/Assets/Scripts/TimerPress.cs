aa4ed2618c62f7c87a3a6a29986e93b0382c06f7
Ronnie Lucero <ronnie_school@yahoo.com>
2021-03-07 20:16:00 -0800

    TimerPress is now universal progress bar script
    Fixed in my scene and wrap-up, still needs testing in Oculus

--- art199_PatientZero/Assets/Scripts/TimerPress.cs
+++ art199_PatientZero/Assets/Scripts/TimerPress.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class TimerPress : MonoBehaviour
 {
 
+    // 1 = Regular progress bar, 2 = Timer Bar Repeated, 3 = Timer Bar One Try
+    public int barType = 1;
+    
     private Image progressBar;
     public GameObject canvas;
-    public bool activated = false;
+
+    private bool activated = false;
+
     public float cap = 0.3f;
     public float timeToHit = 3.0f;
     public float offsetPercent = 10f;
@@ -29,16 +34,20 @@ public class TimerPress : MonoBehaviour
 
                 //this.gameObject.transform.Translate(Vector3.left * (timePassed / (10f * (timeToHit / 3f))));
             }
-            else
+            else if (barType == 2)
             {
                 progressBar.fillAmount = (timeToHit - (time % timeToHit)) / timeToHit;
                 //this.gameObject.transform.Translate(Vector3.right * (timePassed / (10f * (timeToHit / 3f))));
+            }
+            if (progressBar.fillAmount > 0.95 && barType != 2 && time > timeToHit + 1)
+            {
+                deactivateBar();
             }
             
         }
     }
 
-    public void activateTimer()
+    public void activateBar()
     {
         canvas.SetActive(true);
         progressBar = this.transform.Find("Progress Bar").gameObject.GetComponent<Image>();
@@ -46,6 +55,11 @@ public class TimerPress : MonoBehaviour
         time = 0f;
     }
 
+    public void deactivateBar()
+    {
+        canvas.SetActive(false);
+        activated = false;
+    }
     public void hit()
     {
         UnityEngine.Debug.Log(time);
