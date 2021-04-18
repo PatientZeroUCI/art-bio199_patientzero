9de8c76d5f22edf11e044f018be82b83f98cb31b
brendanmiller10 <bjmille2@uci.edu>
2021-04-18 15:03:54 -0700

    Task List Push
    Created a in-game UI for the Task List display. This display uses two custom scripts (Task and TaskListController) to manage the tasks in the scene. Can add and remove tasks using the controller script, which adjusts the display accordingly.

---
+++ art199_PatientZero/Assets/Scripts/Task.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    public enum Status //current status of the task at hand
+    {
+        Inactive = 0,
+        Active = 1,
+        Completed = 2,
+    }
+
+    public string Name; //necessary information that the task manager might need.
+    public string Description;
+    public Status currentStatus;
+    public List<Task> Subtasks;
+
+    private bool subtasks_enabled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Subtasks.Count > 0)
+        {
+            subtasks_enabled = true;
+        }
+    }
+    
+    public bool HasSubtasks()
+    {
+        return subtasks_enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
