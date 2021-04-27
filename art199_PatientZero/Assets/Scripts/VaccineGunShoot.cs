94e5fedb7eec200dcf4c4914febdf6185cdcbee9
Pixsea <jiin@uci.edu>
2021-04-26 18:47:55 -0700

    Added Vaccine gun prefab
    Added Vaccine Gun prefab, has a gun that must be snapped into the port to reload, edited VaccineGunShoot script to have ammo and to reload

--- art199_PatientZero/Assets/Scripts/VaccineGunShoot.cs
+++ art199_PatientZero/Assets/Scripts/VaccineGunShoot.cs
@@ -8,10 +8,71 @@ public class VaccineGunShoot : VRTK.Examples.GunShoot
     public int vaccineType = 0;
     public Material[] materials;
 
+    // Variables sued to control ammo/laoding the vaccine from the port
+    private int ammo = 0;
+    public int reloadAmount = 5; // How much ammo the gun should get when inserted intot he port
+    public GameObject reloadPort;
+
+    public float timer = 0f;  // how long the gun ahs been charging
+    public float chargeTime = 5f;  // How long the gun has to charge to reload
+    private bool charging = false;
+
+
+    // Models that are used depending on if the gun is loaded or not
+    public GameObject emptyGun;
+    public GameObject loadedGun;
+
+
+    private void Start()
+    {
+        chargeTime = chargeTime / Time.fixedDeltaTime;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Debug.Log(timer);
+        if (charging)
+        {
+            // If charging, starting increasing timer
+            timer += 1;
+        }
+        else
+        {
+            // If not charging, remove all charge time
+            timer = 0;
+        }
+
+        // When charged for long enough, reload gun
+        if (timer > chargeTime)
+        {
+            Reload();
+        }
+
+
+        if ((loadedGun != null) && (emptyGun != null))
+        {
+            if (ammo > 0)
+            {
+                loadedGun.SetActive(true);
+                emptyGun.SetActive(false);
+            }
+            else
+            {
+                loadedGun.SetActive(false);
+                emptyGun.SetActive(true);
+            }
+        }
+    }
+
+
     protected override void FireProjectile()
     {
-        if (projectile != null && projectileSpawnPoint != null)
+        if (projectile != null && projectileSpawnPoint != null && (ammo > 0))
         {
+            // Reduce ammo by one
+            ammo -= 1;
+
             GameObject clonedProjectile = Instantiate(projectile, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
             Rigidbody projectileRigidbody = clonedProjectile.GetComponent<Rigidbody>();
             float destroyTime = 0f;
@@ -33,5 +94,38 @@ public class VaccineGunShoot : VRTK.Examples.GunShoot
         if (vaccineType >= 2) { vaccineType = 0; }
 
         transform.Find("Barrel").GetComponent<MeshRenderer>().material = materials[vaccineType];
+    }
+
+    public void Reload()
+    {
+        ammo = reloadAmount;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+        if (reloadPort != null)
+        {
+            if (other.gameObject == reloadPort)
+            {
+                //Reload();
+
+                charging = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+        if (reloadPort != null)
+        {
+            if (other.gameObject == reloadPort)
+            {
+                //Reload();
+
+                charging = false;
+            }
+        }
     }
 }
