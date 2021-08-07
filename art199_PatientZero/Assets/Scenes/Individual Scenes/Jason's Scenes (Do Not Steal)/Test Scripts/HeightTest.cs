4366281c7dd0dc738eff2c6b43b8a61a719487d1
Pixsea <jiin@uci.edu>
2021-08-06 22:00:27 -0700

    Edited Height test individual scene

---
+++ art199_PatientZero/Assets/Scenes/Individual Scenes/Jason's Scenes (Do Not Steal)/Test Scripts/HeightTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightTest : MonoBehaviour
+{
+    public GameObject myObject;
+
+    public OVRCameraRig camRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myObject != null)
+        {
+            Debug.Log(myObject.transform.position.y);
+        }
+        
+
+        if (camRig != null)
+        {
+            Debug.Log(camRig.centerEyeAnchor.transform.position.y + " " + camRig.rightEyeAnchor.transform.position.y + " " + camRig.trackingSpace.transform.position.y);
+        }
+    }
+}
