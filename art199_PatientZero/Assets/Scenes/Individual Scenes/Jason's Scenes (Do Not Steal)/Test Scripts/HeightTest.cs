a1e58a516f098b2fe2a47230d1058a43a56b114c
Pixsea <jiin@uci.edu>
2021-08-09 00:47:59 -0700

    Changed Height Test scene name and edited it

--- art199_PatientZero/Assets/Scenes/Individual Scenes/Jason's Scenes (Do Not Steal)/Test Scripts/HeightTest.cs
+++ art199_PatientZero/Assets/Scenes/Individual Scenes/Jason's Scenes (Do Not Steal)/Test Scripts/HeightTest.cs
@@ -25,7 +25,11 @@ public class HeightTest : MonoBehaviour
 
         if (camRig != null)
         {
-            Debug.Log(camRig.centerEyeAnchor.transform.position.y + " " + camRig.rightEyeAnchor.transform.position.y + " " + camRig.trackingSpace.transform.position.y);
+            //Debug.Log(camRig.centerEyeAnchor.transform.position.y + " " + camRig.rightEyeAnchor.transform.position.y + " " + camRig.trackingSpace.transform.position.y);
+            Debug.Log(camRig.centerEyeAnchor.transform.position.y);
+            //Debug.Log(camRig.rightEyeAnchor.transform.position.y);
+            //Debug.Log(camRig.trackingSpace.transform.position.y);
+
         }
     }
 }
