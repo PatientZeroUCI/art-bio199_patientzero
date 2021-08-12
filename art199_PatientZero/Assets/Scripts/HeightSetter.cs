f76d8793fcf14d17addd0093971cc1883737397a
Pixsea <jiin@uci.edu>
2021-08-12 05:54:43 -0700

    Added HeightSetter Script

---
+++ art199_PatientZero/Assets/Scripts/HeightSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightSetter : MonoBehaviour
+{
+    [SerializeField]
+    private float defaultHeight;
+
+    [SerializeField]
+    private Camera[] cameras;
+
+    [SerializeField]
+    private GameObject[] outerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScaleHeight();
+    }
+
+    void ScaleHeight()
+    {
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            float headHeight = cameras[i].transform.localPosition.y;
+            float newScale = defaultHeight / headHeight;
+            outerTransform[i].transform.localScale = Vector3.one * newScale;
+        }
+    }
+}
