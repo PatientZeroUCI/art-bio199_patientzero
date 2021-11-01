121ef634894d389e583feee678811653d3909a06
hungbuiwork <hungbuiwork@gmail.com>
2021-11-01 15:26:06 -0700

    Hint Script Finished(read desc)
    Hints can now be displayed over VRTK objects. The script "Hint" can be added to the VRTK object, and then the hint written into the "hintInfo" field.
    
    ***For the script to work properly, there must be the "Hint" prefab (located in prefabs) somewhere inside of the scene. The script/ graphics are designed separately so feel free to tweak the "Hint" prefab to look better if you wish~

---
+++ art199_PatientZero/Assets/Scenes/Individual Scenes/Hung's Scenes/Hint.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using VRTK;
+public class Hint : MonoBehaviour
+{
+    ///This class is attached onto VRTK objects, displaying hints above the vrtk object when grabbed.
+    ///Conditions for usage: There must be a gameobject with TextMeshPro in the scene, with the tag "Hint". This gameobject will be repositioned/edited by this script.
+    public string hintInfo  = "Temporary string";
+    private GameObject textObj;
+    private TextMeshPro text;
+    private MeshRenderer textDisplay;
+    private Transform parent;
+    private Transform focus;
+    private Vector3 offset =new Vector3(0, 0.4f, 0);
+    private VRTK_InteractableObject vrtkObject;
+    private bool isDisplayed = false; //bool is used to know when to change text/visibility
+    void Awake()
+    {
+        textObj = GameObject.FindGameObjectWithTag("Hint");
+        text = textObj.GetComponent<TextMeshPro>();
+        textDisplay = textObj.GetComponent<MeshRenderer>();
+        parent = this.transform;
+        focus = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        vrtkObject = this.GetComponent<VRTK_InteractableObject>();
+    }
+
+    void Update()
+    {
+        if (vrtkObject.IsGrabbed())
+        {
+            HintPosition();
+            if (!isDisplayed){
+                NewHint(hintInfo);
+            }
+        }
+
+        else
+        {
+            if (isDisplayed)
+            {
+                HideHint();
+            }
+        }
+    }
+    private void NewHint(string message)
+    {
+        //Changes the text of the hint, then makes it visible
+        isDisplayed = true;
+        text.text = message;
+        textDisplay.enabled = true;
+    }
+    private void HideHint()
+    {
+        //Makes text invisible
+        isDisplayed = false;
+        textDisplay.enabled = false;
+    }
+    private void HintPosition()
+    {
+        //updates the position of the hint object
+        textObj.transform.position = parent.position + offset;
+        textObj.transform.LookAt(2 * textObj.transform.position - focus.position);
+    }
+}
