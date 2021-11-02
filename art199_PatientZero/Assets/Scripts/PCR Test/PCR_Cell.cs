ca88e49d3ab98e9a4a3616885f4bd6d697e5923e
Pixsea <jiin@uci.edu>
2021-11-02 11:03:19 -0700

    Added basic PCR fucntionality
    added ability to create PCR cell grid and rotate cells with use

---
+++ art199_PatientZero/Assets/Scripts/PCR Test/PCR_Cell.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCR_Cell : MonoBehaviour
+{
+    // Whether the path extends in a direction
+    public bool upOpen = false;
+    public bool downOpen = false;
+    public bool leftOpen = false;
+    public bool rightOpen = false;
+
+    // The actual path gameobjects on the hexagon
+    public GameObject upPath;
+    public GameObject downPath;
+    public GameObject leftPath;
+    public GameObject rightPath;
+
+    // Coordinates on the PCR grid it is bottome left
+    public int row = 0;
+    public int column = 0;
+
+    public VRTK.VRTK_InteractableObject linkedObject;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (linkedObject != null)
+        {
+            linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+        }
+
+        UpdatePaths();
+    }
+
+    // Rotates 90 degrees clockwise
+    public void Rotate()
+    {
+        bool temp = upOpen;
+        upOpen = leftOpen;
+        leftOpen = downOpen;
+        downOpen = rightOpen;
+        rightOpen = temp;
+
+        UpdatePaths();
+    }
+
+    // Makes the physical paths appear or dissapear whether they are on or not
+    public void UpdatePaths()
+    {
+        upPath.SetActive(upOpen);
+        downPath.SetActive(downOpen);
+        leftPath.SetActive(leftOpen);
+        rightPath.SetActive(rightOpen);
+    }
+
+    private void Update()
+    {
+        //if (Input.GetKeyDown("space"))
+        //{
+        //    Rotate();
+        //}
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, VRTK.InteractableObjectEventArgs e)
+    {
+        Debug.Log("used");
+        Rotate();
+    }
+}
---
+++ art199_PatientZero/Assets/Scripts/PCR Test/PCR_Grid.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCR_Grid : MonoBehaviour
+{
+    public int width = 4;  // Horizontal size of the grid
+
+    public int height = 8;  // Vertical size of the grid
+
+    private GameObject[,] grid;  //  Array of all the cells
+
+    [SerializeField]
+    private GameObject cellPrefab;
+
+    [SerializeField]
+    private float cellSize;  // Size of each octagon cell
+
+    [SerializeField]
+    private float cellSpacing = .1f;
+
+
+    [SerializeField]
+    private Vector3 bottomLeftCellOffSet;  // The offset of the bottom left cell from this script's position
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new GameObject[width, height];
+
+        // Initialise array to be all null
+        //for (int i = 0; i < width; i++)
+        //{
+        //    for (int j = 0; j < height; j++)
+        //    {
+        //        //grid[i, j] = null;
+
+        //        // Code to make a basic grid
+        //        GameObject newCell = Instantiate(cellPrefab, transform.position + new Vector3(cellSize * i + cellSpacing * i, 0, cellSize * j + cellSpacing * j) + bottomLeftCellOffSet, transform.rotation);
+        //        grid[i, j] = newCell;
+        //    }
+        //}
+
+        // Find all its cellc hildren and put them in the grid array
+        foreach (Transform child in transform)
+        {
+            //child is your child transform
+            PCR_Cell cell = child.GetComponent<PCR_Cell>();
+            // Adjust position
+            child.transform.localPosition = new Vector3(cellSize * cell.row + cellSpacing * cell.row, 0, cellSize * cell.column + cellSpacing * cell.column) + bottomLeftCellOffSet;
+
+            // Add tp array
+            grid[cell.column, cell.row] = child.gameObject;
+        }
+    }
+
+    
+}
