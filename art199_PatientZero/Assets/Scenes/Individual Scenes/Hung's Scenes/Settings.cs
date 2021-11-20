bb098f3468ffd17a03ddfc104207bc5f7eb4fe01
hungbuiwork <hungbuiwork@gmail.com>
2021-11-19 17:33:58 -0800

    Added Tooltips enable/disable to settings menu

---
+++ art199_PatientZero/Assets/Scenes/Individual Scenes/Hung's Scenes/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Settings : MonoBehaviour
+{
+    static public bool showTooltips = true;
+    public TextMeshPro textTooltips;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void switchTooltips()
+    {
+        showTooltips = !showTooltips;
+        textTooltips.text = (showTooltips) ? "On" : "Off";
+    }
+}
