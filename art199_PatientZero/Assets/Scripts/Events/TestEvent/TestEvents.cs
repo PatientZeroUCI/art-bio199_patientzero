83a0ee34754b32331312e69e30a80e61d1468219
Pixsea <jiin@uci.edu>
2022-01-21 06:44:05 -0800

    TEst event scripts

---
+++ art199_PatientZero/Assets/Scripts/Events/TestEvent/TestEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEvents : MonoBehaviour
+{
+    public static TestEvents current;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onEnableBox;
+    public void enableBox()
+    {
+        if (onEnableBox != null)
+        {
+            onEnableBox();
+        }
+    }
+}
