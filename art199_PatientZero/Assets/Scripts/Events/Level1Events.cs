52dda6db021435d0df0c0a662ebde5b6c660fe23
Pixsea <jiin@uci.edu>
2022-01-27 06:57:28 -0800

    Added basic level 1 events

---
+++ art199_PatientZero/Assets/Scripts/Events/Level1Events.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Events : MonoBehaviour
+{
+    public static Level1Events current;
+
+    void Awake()
+    {
+        current = this;
+    }
+
+
+    //  When AI voice finishes reading intro lines
+    public event Action onIntroDone;
+    public void IntroDone()
+    {
+        if (onIntroDone != null)
+        {
+            onIntroDone();
+        }
+    }
+}
