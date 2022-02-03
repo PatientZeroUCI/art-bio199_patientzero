1e73792c59d3732cbb5612db6cb86255dfcb58c6
Pixsea <jiin@uci.edu>
2022-02-03 01:41:06 -0800

    Added help screen to wrap up scene
    Helps creen displays different things depending on the event system, also fixed the bacteria not being grabbable

---
+++ art199_PatientZero/Assets/Scripts/HelpScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScreen : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer screen;
+
+    [SerializeField]
+    private Material GramStain;
+    [SerializeField]
+    private Material DNAMatch;
+    [SerializeField]
+    private Material DNACompare;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Level1Events.current.onSlideInsertedInMicroscope += LoadGramStain;
+        Level1Events.current.onPetriInsertedInMicroscope += LoadDNAMatch;
+        Level1Events.current.onDNAMatched += LoadDNAComapre;
+    }
+
+    void LoadGramStain()
+    {
+        screen.material = GramStain;
+    }
+
+    void LoadDNAMatch()
+    {
+        screen.material = DNAMatch;
+    }
+
+    void LoadDNAComapre()
+    {
+        screen.material = DNACompare;
+    }
+}
