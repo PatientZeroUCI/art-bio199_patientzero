d4b2473c1ca440aee19ed260a1be26893780ab7f
Pixsea <jiin@uci.edu>
2022-02-25 14:01:14 -0800

    Disabled tablet, added rotation for camera, added height adjuster to main scene

--- art199_PatientZero/Assets/Scripts/HeightSetter.cs
+++ art199_PatientZero/Assets/Scripts/HeightSetter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class HeightSetter : MonoBehaviour
 {
@@ -13,6 +14,17 @@ public class HeightSetter : MonoBehaviour
     [SerializeField]
     private GameObject[] outerTransform;
 
+    [SerializeField]
+    private float rotateDegree = 15;
+
+
+    public VRTK_ControllerEvents controllerEventsRight;
+    public VRTK_ControllerEvents.ButtonAlias rightRotateButton = VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress;
+    public VRTK_ControllerEvents controllerEventsLeft;
+    public VRTK_ControllerEvents.ButtonAlias leftRotateButton = VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +35,16 @@ public class HeightSetter : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            ScaleHeight();
+            ChangeHeight(1.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            RotateLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RotateLeft();
         }
     }
 
@@ -38,6 +59,28 @@ public class HeightSetter : MonoBehaviour
             float headHeight = cameras[i].transform.position.y;
             float heightAdjustment = defaultHeight - headHeight;
             outerTransform[i].transform.position += new Vector3(0, heightAdjustment, 0);
+        }
+    }
+
+    public void ChangeHeight(float height)
+    {
+        defaultHeight = height;
+        ScaleHeight();
+    }
+
+    void RotateRight()
+    {
+        for (int i = 0; i < outerTransform.Length; i++)
+        {
+            outerTransform[i].transform.rotation *= Quaternion.Euler(0, rotateDegree, 0);
+        }
+    }
+
+    void RotateLeft()
+    {
+        for (int i = 0; i < outerTransform.Length; i++)
+        {
+            outerTransform[i].transform.rotation *= Quaternion.Euler(0, -rotateDegree, 0);
         }
     }
 }
