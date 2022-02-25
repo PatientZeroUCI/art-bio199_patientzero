1c712d2ebcc9fee9e9e8a758b25322133a755d40
Daniel Boghossian <dboghoss@uci.edu>
2022-02-24 23:47:17 -0800

    Semi working elevator doors

---
+++ art199_PatientZero/Assets/Scripts/ElevatorDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoor : MonoBehaviour
+{
+    public bool isLeftDoor;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isLeftDoor)
+        {
+            transform.position = transform.position - new Vector3(0.6f, 0, 0);
+        }
+        else
+        {
+            transform.position = transform.position + new Vector3(0.6f, 0, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isLeftDoor)
+        {
+            transform.position = transform.position + new Vector3(0.6f, 0, 0);
+        }
+        else
+        {
+            transform.position = transform.position - new Vector3(0.6f, 0, 0);
+        }
+    }
+}
