05203ec21ae6288682f85b84fba2a9eeecb86d86
ArianiBobani <amertz@uci.edu>
2022-03-05 16:19:28 -0800

    Added multiple sfx and bgm
    - New main menu music put in
    - Flashlight now has a sound for picking it up
    - Papers now have sounds for picking them up

---
+++ art199_PatientZero/Assets/flashlightAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class flashlightAudio : MonoBehaviour
+{
+    public AudioSource flashlightSound;
+    public AudioClip [] flashlightClips;
+
+    public VRTK_InteractableObject flashlight;
+
+    bool yoink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yoink = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flashlight.IsGrabbed() && yoink) {
+            int clipNum = Random.Range(0,2); //picks a random number between 1 and 3
+            flashlightSound.clip = flashlightClips[clipNum];
+            flashlightSound.Play();
+            yoink = false;
+        }
+        else if (!flashlight.IsGrabbed() && !yoink) {
+            yoink = true;
+        }
+    }
+}
---
+++ art199_PatientZero/Assets/paperAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class paperAudio : MonoBehaviour
+{
+    public AudioSource paperSound;
+
+    public VRTK_InteractableObject paper;
+
+    bool yoink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yoink = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paper.IsGrabbed() && yoink) {
+            paperSound.Play();
+            yoink = false;
+        }
+        else if (!paper.IsGrabbed() && !yoink) {
+            yoink = true;
+        }
+    }
+}
