6fa42d1459640dfab13ff17a6db6db4d04d459ea
Daniel Boghossian <dboghoss@uci.edu>
2022-04-10 16:54:18 -0700

    Elevator Progress

---
+++ art199_PatientZero/Assets/Scripts/InsideElevator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideElevator : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("hello");
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Debug.Log("goodbye");
+    }
+}
